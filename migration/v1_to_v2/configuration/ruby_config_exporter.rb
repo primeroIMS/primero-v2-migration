ec39a038d3be5652348633c291bbcea1287da44f
Ron Elledge <relledge@quoininc.com>
2021-02-23 17:15:30 -0500

    R2-1082 update for field name changes

--- migration/v1_to_v2/configuration/ruby_config_exporter.rb
+++ migration/v1_to_v2/configuration/ruby_config_exporter.rb
@@ -485,17 +485,28 @@ def configuration_hash_form_section(object)
   config_hash
 end
 
+def configuration_hash_field_name(config_hash, form_unique_id)
+  if form_unique_id == 'notes_section'
+    config_hash['name'] = 'note_date' if config_hash['name'] == 'notes_date'
+    config_hash['name'] = 'note_text' if config_hash['name'] == 'field_notes_subform_fields'
+  end
+  config_hash['name'] = 'other_documents' if config_hash['name'] == 'upload_other_document'
+  config_hash['name'] = 'status' if %w[child_status inquiry_status].include?(config_hash['name'])
+  config_hash['name'] = 'unhcr_export_opt_in' if config_hash['name'] == 'unhcr_export_opt_out'
+  config_hash['name'] = config_hash['name'].gsub(/bia/, 'assessment') if config_hash['name'].include?('bia_approved')
+  config_hash['name'] = 'approval_status_assessment' if config_hash['name'] == 'approval_status_bia'
+  config_hash['name'] = 'short_id' if config_hash['name'] == 'cp_short_id'
+  config_hash
+end
+
 def configuration_hash_field(field, collapsed_fields, form_unique_id)
   config_hash = field.attributes.except('id', 'highlight_information', 'base_language', 'deletable', 'searchable_select',
                                         'create_property', 'subform_section_id').with_indifferent_access
   config_hash['collapsed_field_for_subform_unique_id'] = form_unique_id if collapsed_fields.include?(field.name)
   config_hash['subform_unique_id'] = field.subform_section_id if field.type == 'subform'
   config_hash['disabled'] = false if field.type.include?('upload_box')
-  if form_unique_id == 'notes_section'
-    config_hash['name'] = 'note_date' if config_hash['name'] == 'notes_date'
-    config_hash['name'] = 'note_text' if config_hash['name'] == 'field_notes_subform_fields'
-  end
   config_hash['option_strings_source'] = field.option_strings_source.split(' ').first if field.option_strings_source&.include?('use_api')
+  config_hash = configuration_hash_field_name(config_hash, form_unique_id)
   config_hash
 end
 
