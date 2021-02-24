b1a56361fba83dbe41ff64358c243ff0c5dafdbe
Alberto Espinoza <aespinoz@quoininc.com>
2021-02-24 14:53:11 -0600

    R2-1310 Attachment exporter
    - Make export a class
    - Generating ruby file to be run on v2

--- migration/v1_to_v2/attachments/attachment_exporter.rb
+++ migration/v1_to_v2/attachments/attachment_exporter.rb
@@ -2,84 +2,175 @@
 
 require 'fileutils'
 
-def export_file(files, folder, record_data, form)
-  files.each do |key, value|
-    name = value.is_a?(String) ? value : value.file_name
-    path_folder = "#{folder}/#{@record_id}/#{form}"
+# rubocop:disable Metrics/ClassLength
+# Class that get record's attachments and generate files to be inserted
+class AttachmentExporter
+  RECORD_TYPES = {
+    'case' => 'Child',
+    'incident' => 'Incident',
+    'tracing_request' => 'TracingRequest'
+  }.freeze
 
-    FileUtils.mkdir_p(path_folder)
-    File.open("#{path_folder}/#{name}", 'wb') do |f|
-      f.write(record_data.fetch_attachment(key))
+  ATTACHMENTS_FORMS = {
+    'case' => %w[photo_keys bia_documents bid_documents other_documents],
+    'incident' => [],
+    'tracing_request' => []
+  }.freeze
+
+  FIELD_MAPPING = {
+    'audio_attachments' => 'recorded_audio',
+    'photo_keys' => 'current_photo_key',
+    'bia_documents' => 'upload_bia_document',
+    'bid_documents' => 'upload_bid_document',
+    'other_documents' => 'upload_other_document'
+  }.freeze
+
+  def initialize(options = {})
+    @export_dir = options[:base_folder] || 'record-data-files'
+    @batch_size = options[:batch_size] || 500
+    @json_to_export = {}
+    FileUtils.mkdir_p(@export_dir)
+  end
+
+  def export
+    ATTACHMENTS_FORMS.each do |type, forms|
+      @record_type = RECORD_TYPES[type].constantize
+
+      next if forms.empty? || @record_type.count.zero?
+
+      type_folder = "#{type.pluralize}-attachments"
+      folder_to_save = "#{@export_dir}/#{type_folder}"
+
+      FileUtils.mkdir_p(folder_to_save)
+
+      puts "Importing #{@record_type.count} #{type.pluralize}"
+      export_records(forms, type, folder_to_save)
     end
   end
-end
 
-def build_data_to_json(files, folder, form)
-  # binding.pry
-  @json_to_export[@record_type.to_s][@record_id][form] = [] if files.present?
-  files.each do |key, value|
-    name = value.is_a?(String) ? value : value.file_name
-    path_folder = "#{folder}/#{@record_id}/#{form}"
+  private
 
-    @json_to_export[@record_type.to_s][@record_id][form] << {
-      id: key,
-      filename: name,
-      path: "#{path_folder}/#{name}"
-    }
+  def set_record_id(type, record_id)
+    @record_id = record_id
+    @json_to_export[type][@record_id] = {}
   end
-end
 
-def get_files_to_export(files, form)
-  return [] if files.empty?
+  def export_records(forms, type, folder_to_save)
+    sufix = 0
+    @record_type.each_slice(@batch_size) do |records|
+      @json_to_export[type] = {}
+      records.each do |record|
+        next if record&._attachments&.empty?
 
-  return { files.values.last => "#{files.values.last}.#{files.keys.last}" } if form == 'audio_attachments'
-  return files.reduce({}) { |acc, doc| acc.merge(doc => doc) } if form == 'photo_keys'
+        set_record_id(type, record._id)
+        export_forms_attachments(forms, record, folder_to_save, type)
+      end
+      build_file(@record_type.to_s, sufix += 1)
+    end
+  end
 
-  files.reduce({}) { |acc, doc| acc.merge(doc.attachment_key => doc) }
-end
+  def export_forms_attachments(forms, record, folder_to_save, type)
+    forms.each do |form|
+      files = get_files_to_export(record.send(form), form)
 
-BASE_FOLDER = ARGV[0] || 'files-to-export'
-ATTACHMENTS_FORMS = {
-  'Child' => %w[audio_attachments photo_keys bia_documents bid_documents other_documents],
-  'Incident' => [],
-  'TracingRequest' => []
-}.freeze
+      next if files.empty?
 
-FileUtils.mkdir_p(BASE_FOLDER)
-ATTACHMENTS_FORMS.each do |type, forms|
-  @record_type = type.constantize
-  @json_to_export = {}
+      build_data_to_attach(files, folder_to_save, type, form)
 
-  next if forms.empty? || @record_type.count.zero?
+      export_file(files, folder_to_save, record, form)
+    end
+  end
 
-  folder_to_save = "#{BASE_FOLDER}/#{type}"
+  def get_files_to_export(files, form)
+    return [] if files.empty?
 
-  FileUtils.mkdir_p(folder_to_save)
+    return { files.values.last => "#{files.values.last}.#{files.keys.last}" } if form == 'audio_attachments'
+    return files.reduce({}) { |acc, doc| acc.merge(doc => doc) } if form == 'photo_keys'
 
-  @json_to_export[type] = {}
+    files.reduce({}) { |acc, doc| acc.merge(doc.attachment_key => doc) }
+  end
 
-  puts "Importing #{@record_type.count} #{type.pluralize}"
-  @record_type.each_slice(100) do |records|
-    records.each do |record|
-      next if record&._attachments&.empty?
+  def build_data_to_attach(files, folder, type, form)
+    @json_to_export[type][@record_id][form] = []
+    files.each do |_key, value|
+      name = value.is_a?(String) ? value : value.file_name
 
-      @record_id = record._id
-      @json_to_export[type][@record_id] = {}
-      # binding.pry
+      @json_to_export[type][@record_id][form] << {
+        record_type: @record_type.to_s, record_id: @record_id, field_name: form,
+        file_name: name, date: value.try(:date), comments: value.try(:comments),
+        is_current: value.try(:is_current) || false, description: value.try(:document_description),
+        path: "#{folder}/#{@record_id}/#{form}/#{name}"
+      }
+    end
+  end
 
-      forms.each do |form|
-        files_data = record.send(form)
-        files = get_files_to_export(files_data, form)
+  def export_file(files, folder, record_data, form)
+    files.each do |key, value|
+      name = value.is_a?(String) ? value : value.file_name
+      path_folder = "#{folder}/#{@record_id}/#{form}"
 
-        build_data_to_json(files, folder_to_save, form)
-
-        export_file(files, folder_to_save, record, form)
+      FileUtils.mkdir_p(path_folder)
+      File.open("#{path_folder}/#{name}", 'wb') do |f|
+        f.write(record_data.fetch_attachment(key))
       end
     end
   end
 
-  File.open("#{BASE_FOLDER}/#{type}.#{DateTime.now.strftime('%Y%m%d.%H%M')}.json", 'w') do |f|
-    f.write(JSON.pretty_generate(@json_to_export))
+  def get_field_name(attachement_form)
+    FIELD_MAPPING[attachement_form]
   end
 
+  def mime_type(path)
+    `file --brief --mime-type #{path}`.strip
+  end
+
+  def get_attachment_type(path)
+    mime = mime_type(path)
+    return 'audio' if mime.start_with?('audio')
+    return 'image' if mime.start_with?('image')
+
+    'document'
+  end
+
+  def initialize_script_for_attachment(type, sufix)
+    @output = File.new("#{@export_dir}/#{type}.#{sufix}.rb", 'w')
+    @output.puts("# Automatically generated script to migrate attachment from v1.7 to v2.0+\n\n")
+  end
+
+  def add_attachment_type_to_file_for_attachment(path)
+    @output.puts "attachement.attachment_type = '#{get_attachment_type(path)}'"
+  end
+
+  def add_date_to_file_for_attachment(date)
+    return unless date.present?
+
+    @output.puts "attachement.date = \"#{date.strftime('%Y-%m-%d')}\""
+  end
+
+  def write_script_for_attachment(form_name, files)
+    files.each do |data|
+      @output.puts "puts 'Inserting \"#{get_attachment_type(data[:path])}\" to #{data[:record_id]})'"
+      @output.puts "attachement = Attachment.new(#{data.except(:path, :field_name, :date)})"
+      add_date_to_file_for_attachment(data[:date])
+      @output.puts "attachement.record_type = #{data[:record_type]}"
+      add_attachment_type_to_file_for_attachment(data[:path])
+      @output.puts "attachement.field_name = '#{get_field_name(form_name)}'"
+      @output.puts "attachement.file.attach(io: File.open('#{data[:path]}'), filename: '#{data[:file_name]}')"
+      @output.puts "attachement.save!\n\n\n"
+    end
+  end
+
+  def build_file(type, sufix)
+    initialize_script_for_attachment(type, sufix)
+    RECORD_TYPES.keys.each do |key|
+      next if @json_to_export[key].blank?
+
+      @json_to_export[key].values.each do |value|
+        value.each do |form_name, files|
+          write_script_for_attachment(form_name, files)
+        end
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/ClassLength
