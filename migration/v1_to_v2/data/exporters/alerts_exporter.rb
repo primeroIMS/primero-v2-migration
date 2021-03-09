dfb5138894c13e80c5324fb6862cc267a8954537
Dennis Hernandez <dhernand@quoininc.com>
2021-03-09 12:12:34 -0600

    Reuse code from DataExporter

--- migration/v1_to_v2/data/exporters/alerts_exporter.rb
+++ migration/v1_to_v2/data/exporters/alerts_exporter.rb
@@ -4,12 +4,19 @@ require_relative('data_exporter.rb')
 
 # Generates a v2.0+ compatible script to create alerts.
 class AlertsExporter < DataExporter
-  def initialize(options = {})
-    super(options)
-    @log = options[:log] || fallback_logger
+  private
+
+  def model_class(_record_type)
+    'Alert'
   end
 
-  private
+  def object_query(_object_name)
+    Child
+  end
+
+  def data_object_names
+    %w[Alert]
+  end
 
   # rubocop:disable Style/StringLiterals
 
@@ -20,7 +27,7 @@ class AlertsExporter < DataExporter
     ].join("\n").freeze
   end
 
-  def ending(_)
+  def ending(_object_name)
     [
       "]\n",
       "alerts.each do |alert|",
@@ -37,73 +44,37 @@ class AlertsExporter < DataExporter
 
   # rubocop:enable Style/StringLiterals
 
-  def file_for(_, index)
-    config_dir = "#{@export_dir}/alerts"
-    FileUtils.mkdir_p(config_dir)
-    "#{config_dir}/alerts#{index}.rb"
-  end
+  def object_hashes(_object_name, objects)
+    objects.map do |object|
+      next unless object.alerts.present? && object.alerts.is_a?(Array)
 
-  def fallback_logger
-    timestamp = DateTime.now.strftime('%Y%m%d.%I%M')
-    log_filename = "alerts-exporter-logs-#{timestamp}.txt"
-    log = Logger.new(log_filename)
-    log.formatter = proc do |severity, _, _, msg|
-      "#{severity}: #{msg}\n"
-    end
-    log
-  end
-
-  def object_hashes(_, objects)
-    hashes = objects.map do |object|
       object.alerts.map do |alert|
         alert_hash = alert.to_hash
+        alert_hash['user'] = user_string(alert_hash)
+        alert_hash['agency'] = agency_string(alert_hash)
+        alert_hash['date'] = date_string(alert_hash)
         alert_hash['record_id'] = uuid_format(object.id)
         alert_hash['record_type'] = object.class.name
         alert_hash
       end
-    end
-
-    hashes.flatten
+    end.compact.flatten
   end
 
-  def export_data_objects(object_name)
-    puts 'Exporting alerts...'
-    super(object_name)
-  end
+  def date_string(object_hash)
+    return unless object_hash['date'].present?
 
-  def data_object_names
-    %w[Case]
-  end
-
-  # rubocop:disable Style/StringLiterals
-
-  def config_to_ruby_string(object_hash, _)
-    [
-      "  Alert.new(",
-      "    alert_for: \"#{object_hash['alert_for']}\",",
-      "    type: \"#{object_hash['type']}\",",
-      "    date: Date.parse(\"#{object_hash['date']}\"),",
-      "    unique_id: \"#{object_hash['unique_id']}\",",
-      "    form_sidebar_id: \"#{object_hash['form_sidebar_id']}\",",
-      user_string(object_hash),
-      agency_string(object_hash),
-      "    record_id: \"#{object_hash['record_id']}\",",
-      "    record_type: \"#{object_hash['record_type']}\"",
-      "  ),\n"
-    ].compact.join("\n")
+    "Date.parse(\"#{object_hash['date']}\")"
   end
 
   def user_string(object_hash)
     return unless object_hash['user'].present?
 
-    "    user: User.find_by(user_name: \"#{object_hash['user']}\"),"
+    "User.find_by(user_name: \"#{object_hash['user']}\"),"
   end
 
   def agency_string(object_hash)
     return unless object_hash['agency'].present?
 
-    "    agency: Agency.find_by(unique_id: \"#{object_hash['agency']}\"),"
+    "Agency.find_by(unique_id: \"#{object_hash['agency']}\"),"
   end
-
-  # rubocop:enable Style/StringLiterals
 end
