77c9030b23b91200bbf86f3fc7fa3071c8d88b47
Ron Elledge <relledge@quoininc.com>
2021-05-28 09:09:56 -0400

    PRIM-2435 fix reporting_location admin_level_map in base_config_exporter

--- migration/v1_to_v2/configuration/exporters/base_config_exporter.rb
+++ migration/v1_to_v2/configuration/exporters/base_config_exporter.rb
@@ -29,8 +29,9 @@ class BaseConfigExporter < ConfigurationExporter
   end
 
   def convert_reporting_location_config(reporting_location_config)
-    reporting_location_hash = reporting_location_config.attributes.except('admin_level_map', 'reg_ex_filter')
+    reporting_location_hash = reporting_location_config.attributes.except('admin_level_map', 'reg_ex_filter', 'label_key')
     reporting_location_hash['admin_level_map'] = reporting_location_config.admin_level_map.map { |k, v| [v, [k]] }.to_h
+    reporting_location_hash['admin_level_map'][reporting_location_config.admin_level] = [reporting_location_config.label_key]
     reporting_location_hash
   end
 
