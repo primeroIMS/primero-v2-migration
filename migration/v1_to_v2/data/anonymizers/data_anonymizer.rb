3904b3d7c38258c68581b2f11036f3870b4b602d
Ron Elledge <relledge@quoininc.com>
2021-06-15 12:06:48 -0400

    R2-1285 fixed issue with relation_name in data_anonymizer

--- migration/v1_to_v2/data/anonymizers/data_anonymizer.rb
+++ migration/v1_to_v2/data/anonymizers/data_anonymizer.rb
@@ -84,7 +84,7 @@ class DataAnonymizer
       'telephone_caregiver' => 'phone',
       'telephone_caregiver_future' => 'phone',
       'national_id_no' => 'id',
-      'family_details_section||relation_name' => 'name',
+      'family_details_section||relation_name' => 'full_name',
       'family_details_section||relation_identifiers' => 'id',
       'family_details_section||relation_nickname' => 'first_name',
       'family_details_section||relation_address_current' => 'address',
