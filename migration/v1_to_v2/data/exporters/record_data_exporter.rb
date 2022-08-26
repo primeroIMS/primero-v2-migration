121002ef9e26f10dee2b8a1ba32f614821c11d3e
ajit.shetty <ajit.shetty@trigyn.com>
2022-08-26 13:08:49 +0530

    [Update] added the radio button fix

--- migration/v1_to_v2/data/exporters/record_data_exporter.rb
+++ migration/v1_to_v2/data/exporters/record_data_exporter.rb
@@ -6,6 +6,11 @@ require_relative('data_exporter.rb')
 class RecordDataExporter < DataExporter
   private
 
+  def initialize(options = {})
+    super(options)
+    @radio_fields = get_radio_fields
+  end
+
   def parse_object(object)
     super(object).merge(ownership_fields(object))
   end
@@ -14,6 +19,46 @@ class RecordDataExporter < DataExporter
     notes.each do |note|
       note['note_date'] = note.delete('notes_date')
       note['note_text'] = note.delete('field_notes_subform_fields')
+    end
+  end
+
+  def get_radio_fields
+    primero_module = PrimeroModule.find_by_name("CP")  #hardcoded for now. need to change it to accomodate either one. Cannot use all since there comflicting fields in both modules
+    formsections   = primero_module.associated_forms(true)
+    radio_fields   = []
+    formsections.each do |form|
+      radio_fields << form.fields.map{ |s| s.name if s.visible == true && s.type == "radio_button" && s.option_strings_source == "lookup lookup-yes-no" }.compact
+    end
+    return radio_fields.flatten.compact
+  end
+
+  def value_to_ruby_string(value, include_blank = false)
+    return 'nil' if include_blank && value.nil?
+
+    puts value
+
+    if value.is_a?(Hash)
+      ruby_string = "{\n"
+      _i
+      ruby_string += i
+      # TODO: was using .compact instead of .reject but it was throwing away false values.  We want to keep those
+      ruby_string += (include_blank ? value : value.reject { |_, v| v.nil? || v == [] }).map do |k,v|
+        if @radio_fields.include?(k)
+          "#{key_to_ruby(k)}: \"#{v}\""
+        else
+          "#{key_to_ruby(k)}: #{value_to_ruby_string(v, include_blank)}"
+        end
+      end.join(",\n#{i}")
+      i_
+      ruby_string + "\n#{i}}"
+    elsif value.is_a?(Array)
+      array_value_to_ruby_string(value, include_blank)
+    elsif value.is_a?(Range)
+      value
+    elsif value.is_a?(String) && (value.include?('.parse(') || value.include?('.find_by('))
+      value
+    else
+      value.to_json
     end
   end
 
